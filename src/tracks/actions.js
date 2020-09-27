bcc37cad6798ce55aaf2a3a31ba620f1c7274e81
Chris Padilla <cdpadilla42@gmail.com>
2020-09-27 18:27:57 -0500

    Write action creator and action constant

---
+++ src/tracks/actions.js
@@ -0,0 +1,7 @@
+export const GET_TRACKS = 'GET_TRACKS';
+
+export function getTracks() {
+  return {
+    type: GET_TRACKS,
+  };
+}
--- src/tracks/reducer.js
+++ src/tracks/reducer.js
@@ -1,7 +1,11 @@
+import { GET_TRACKS } from './actions';
+
 const defaultState = { tracks: [] };
 
 const reducer = (state = defaultState, { type }) => {
   switch (type) {
+    case GET_TRACKS:
+      return { ...state, tracks: ['got'] };
     default:
       return state;
   }
