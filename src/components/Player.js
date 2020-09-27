e12b1d29146f37742fe2ae59dd066327d76e092a
Chris Padilla <cdpadilla42@gmail.com>
2020-09-27 18:34:33 -0500

    Add useEffect hook

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TrackDisplay from '../tracks/TrackDisplay';
 import Playlist from './Playlist';
 
+// TODO Bring in action creator to run on component mount to get the data
+
 const Player = () => {
+  useEffect(() => {
+    console.log('hey');
+  }, []);
+
   // size 490 x 300
   return (
     <div className="player">
