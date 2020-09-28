d6d5bfe12ab7e541c493bf6bf87eede85ff7745c
Chris Padilla <cdpadilla42@gmail.com>
2020-09-27 19:07:58 -0500

    Add getTracks to useEffect hook.

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,33 @@
 import React, { useEffect } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import TrackDisplay from '../tracks/TrackDisplay';
 import Playlist from './Playlist';
+import { getTracks } from '../tracks/actions';
 
 // TODO Bring in action creator to run on component mount to get the data
 
-const Player = () => {
+const Player = ({ getTracks }) => {
   useEffect(() => {
-    console.log('hey');
+    getTracks();
   }, []);
 
   // size 490 x 300
   return (
     <div className="player">
+      <button onClick={getTracks}>Hey</button>
       <TrackDisplay />
       <Playlist />
     </div>
   );
 };
 
-export default Player;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ getTracks }, dispatch);
+}
+
+function mapStateToProps(state) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
