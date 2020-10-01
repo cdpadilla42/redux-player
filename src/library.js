1f1b999ef071156fccd564610c404911dccc7370
Chris Padilla <cdpadilla42@gmail.com>
2020-10-01 13:28:16 -0500

    Add lib function to format time

--- src/library.js
+++ src/library.js
@@ -2,10 +2,17 @@ export function convertTime(time) {
   let minutes = 0;
   let seconds = 0;
 
-  if (time % 60 > 0) {
+  if (time % 60 >= 0) {
     minutes = Math.floor(time / 60);
     seconds = time - minutes * 60;
+  } else {
+    seconds = time;
   }
 
-  return `${minutes}:${seconds}`;
+  return `${minutes}:${formatValue(seconds)}`;
+}
+
+function formatValue(n) {
+  if (n < 10) return `0${n}`;
+  return n;
 }
